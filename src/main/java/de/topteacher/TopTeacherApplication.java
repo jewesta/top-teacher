aec941e5f8c2338993375dd8763fd5abf90ed16f
jewesta <>
2026-06-04 22:53:44 +0200

    #2 Moved styles.css to theme folder for live-update

--- src/main/java/de/topteacher/TopTeacherApplication.java
+++ src/main/java/de/topteacher/TopTeacherApplication.java
@@ -3,13 +3,11 @@ package de.topteacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
-@StyleSheet(Lumo.STYLESHEET)
-@CssImport("./styles.css")
+@Theme(value = "topteacher", variant = Lumo.LIGHT)
 @SpringBootApplication
 public class TopTeacherApplication implements AppShellConfigurator {
 
