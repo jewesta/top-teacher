ec267e10c26be3c32b1afae0879f7e98eeb919d5
jewesta <>
2026-06-04 17:22:12 +0200

    #2 Formatting

--- src/main/java/de/topteacher/model/CoursePeriod.java
+++ src/main/java/de/topteacher/model/CoursePeriod.java
@@ -1,6 +1,7 @@
 package de.topteacher.model;
 
 public enum CoursePeriod {
+
 	FIRST_HALF("1. Hj."), SECOND_HALF("2. Hj."), FULL_YEAR("Ganzjahr");
 
 	private final String displayName;
@@ -12,4 +13,5 @@ public enum CoursePeriod {
 	public String getDisplayName() {
 		return displayName;
 	}
+
 }
