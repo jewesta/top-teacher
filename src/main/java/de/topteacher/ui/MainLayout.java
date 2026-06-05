70cb2407645fff664950980a4336a8be67eebba5
jewesta <>
2026-06-05 03:27:25 +0200

    #2 navigation now no longer a left bar but on top as tabs

--- src/main/java/de/topteacher/ui/MainLayout.java
+++ src/main/java/de/topteacher/ui/MainLayout.java
@@ -2,19 +2,16 @@ package de.topteacher.ui;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.sidenav.SideNav;
-import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
-import com.vaadin.flow.router.HasDynamicTitle;
-import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
 
 import de.topteacher.ui.view.CoursesView;
 import de.topteacher.ui.view.ExamsView;
@@ -22,61 +19,49 @@ import de.topteacher.ui.view.PupilsView;
 
 public class MainLayout extends AppLayout implements AfterNavigationObserver {
 
-	private final H2 viewTitle = new H2();
+	private final Tabs navigationTabs = new Tabs();
+	private final Tab pupilsTab = navigationTab("Schüler", VaadinIcon.ACADEMY_CAP, PupilsView.class);
+	private final Tab coursesTab = navigationTab("Kurse", VaadinIcon.BOOK, CoursesView.class);
+	private final Tab examsTab = navigationTab("Klausuren", VaadinIcon.EDIT, ExamsView.class);
 
 	public MainLayout() {
-		setPrimarySection(Section.DRAWER);
-
 		addToNavbar(createHeader());
-		addToDrawer(createDrawerHeader(), createNavigation());
 	}
 
 	@Override
 	public void afterNavigation(final AfterNavigationEvent event) {
-		viewTitle.setText(getCurrentPageTitle());
+		final Component content = getContent();
+		if (content instanceof PupilsView) {
+			navigationTabs.setSelectedTab(pupilsTab);
+		} else if (content instanceof CoursesView) {
+			navigationTabs.setSelectedTab(coursesTab);
+		} else if (content instanceof ExamsView) {
+			navigationTabs.setSelectedTab(examsTab);
+		}
 	}
 
 	private Component createHeader() {
-		final DrawerToggle drawerToggle = new DrawerToggle();
-
-		viewTitle.addClassName("tt-view-title");
-
-		final HorizontalLayout header = new HorizontalLayout(drawerToggle, viewTitle);
+		final Header header = new Header();
 		header.addClassName("tt-header");
 
+		final Image logo = new Image("images/topteacher-logo.png", "TopTeacher!");
+		logo.addClassName("tt-app-logo");
+
+		navigationTabs.addClassName("tt-top-navigation");
+		navigationTabs.add(pupilsTab, coursesTab, examsTab);
+
+		final HorizontalLayout spacer = new HorizontalLayout();
+		spacer.addClassName("tt-header-spacer");
+
+		header.add(logo, navigationTabs, spacer);
 		return header;
 	}
 
-	private Component createDrawerHeader() {
-		final Header header = new Header();
-		header.addClassName("tt-drawer-header");
-
-		final Image appName = new Image("images/topteacher-logo.png", "TopTeacher!");
-		appName.addClassName("tt-app-logo");
-
-		header.add(appName);
-		return header;
-	}
-
-	private Component createNavigation() {
-		final SideNav navigation = new SideNav();
-		navigation.addClassName("tt-side-nav");
-		navigation.addItem(new SideNavItem("Schüler", PupilsView.class, VaadinIcon.ACADEMY_CAP.create()));
-		navigation.addItem(new SideNavItem("Kurse", CoursesView.class, VaadinIcon.BOOK.create()));
-		navigation.addItem(new SideNavItem("Klausuren", ExamsView.class, VaadinIcon.EDIT.create()));
-
-		final Main wrapper = new Main(navigation);
-		wrapper.addClassName("tt-navigation");
-		return wrapper;
-	}
-
-	private String getCurrentPageTitle() {
-		final Component content = getContent();
-		if (content instanceof final HasDynamicTitle dynamicTitle) {
-			return dynamicTitle.getPageTitle();
-		}
-
-		final PageTitle title = content.getClass().getAnnotation(PageTitle.class);
-		return title == null ? "TopTeacher" : title.value();
+	private Tab navigationTab(final String label, final VaadinIcon icon, final Class<? extends Component> view) {
+		final RouterLink link = new RouterLink();
+		link.addClassName("tt-top-navigation-link");
+		link.setRoute(view);
+		link.add(icon.create(), new Span(label));
+		return new Tab(link);
 	}
 }
